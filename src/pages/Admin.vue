a0feb3f8066c35c55b7ec93a3f27f3d7754cfbf7
JMagana <jmagana.9824@gmail.com>
2019-09-07 21:19:59 -0600

    Inicio Vista Mesas y Admin

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -1,13 +1,57 @@
 <template>
-  
+  <div class="container">
+  <div class="row">
+    <div class="col">
+     <ul class="navbar-nav ml-auto">
+          <sidebar-link to="/admin/admin/mesas">
+            <i  class="btn btn-success btn-sm" role="button">Mesas</i>
+          </sidebar-link>
+          </ul>
+    </div>
+    <div class="col">
+     <ul class="navbar-nav ml-auto">
+          <sidebar-link to="/admin/admin/productos">
+            <i class="btn btn-primary btn-sm" role="button">Productos</i>
+          </sidebar-link>
+          </ul>
+    </div>
+    <div class="col">
+      <ul class="navbar-nav ml-auto">
+          <sidebar-link to="/admin/admin/usuario-rol">
+            <i class="btn btn-primary btn-sm" role="button">Usuarios y Roles</i>
+          </sidebar-link>
+          </ul>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+     <ul class="navbar-nav ml-auto">
+          <sidebar-link to="/admin/admin/categorias">
+            <i  class="btn btn-success btn-sm" role="button">Categorías</i>
+          </sidebar-link>
+          </ul>
+    </div>
+    <div class="col">
+     <ul class="navbar-nav ml-auto">
+          <sidebar-link to="/admin/admin/compras">
+            <i class="btn btn-primary btn-sm" role="button">Compras</i>
+          </sidebar-link>
+          </ul>
+    </div>
+    <div class="col">
+      <ul class="navbar-nav ml-auto">
+          <sidebar-link to="/admin/admin/parametros">
+            <i class="btn btn-primary btn-sm" role="button">Parámetros</i>
+          </sidebar-link>
+          </ul>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- src/pages/AdminMesas.vue
+++ src/pages/AdminMesas.vue
@@ -1,13 +1,73 @@
 <template>
-  
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-8">
+          <card id="card" class="table-wrapper-scroll-y my-custom-scrollbar" body-classes="table-">
+            <template slot="header">
+              <h4 class="card-title">Mesas</h4>
+            </template>
+            <l-table
+              class="table-hover table-striped"
+              :columns="table.columns"
+              :data="table.data"
+            ></l-table>
+          </card>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import ChartCard from "src/components/Cards/ChartCard.vue";
+import StatsCard from "src/components/Cards/StatsCard.vue";
+import LTable from "src/components/Table.vue";
+import Card from "src/components/Cards/Card.vue";
+const tableColumns = [
+  "id",
+  "Mesa"
+];
+const tableData = [
+  {
+    id: 1,
+    mesa: "Uno"
+  },
+  {
+    id: 2,
+    mesa: "Dos"
+  },
+  {
+    id: 3,
+    mesa: "Tres"
+  },
+  {
+    id: 4,
+    mesa: "Cuatro"
+  },
+  {
+    id: 5,
+    mesa: "Cinco"
+  }
+];
 
-}
+export default {
+  components: {
+    LTable,
+    ChartCard,
+    StatsCard,
+    Card
+  },
+  data() {
+    return {
+      table: {
+        columns: [...tableColumns],
+        data: [...tableData]
+      }
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
