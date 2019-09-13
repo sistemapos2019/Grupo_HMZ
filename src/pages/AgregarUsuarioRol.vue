45a16c74d06f181c4365f45517b43f218b0faaee
JMagana <jmagana.9824@gmail.com>
2019-09-13 00:59:00 -0600

    add User

---
+++ src/pages/AgregarUsuarioRol.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-8">
+          <edit-profile-form>
+          </edit-profile-form>
+        </div>
+        <div class="col-md-4">
+          <user-card>
+          </user-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import AddProfileForm from './UserProfile/AddProfileForm.vue'
+
+
+  export default {
+    components: {
+      AddProfileForm,
+    }
+  }
+
+</script>
+<style>
+
+</style>
