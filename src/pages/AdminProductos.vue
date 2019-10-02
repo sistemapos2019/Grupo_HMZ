b03236106d9f6e5b3c4f89934ac646acca5f3b69
benjamin lopez <benlop27@icloud.com>
2019-10-01 23:36:13 -0600

    Se incluye admin de productos

--- src/pages/AdminProductos.vue
+++ src/pages/AdminProductos.vue
@@ -11,44 +11,12 @@
       </div>
       <br>
       <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-12">
           <card id="card" class="table-wrapper-scroll-y my-custom-scrollbar" body-classes="table-">
-            <template slot="header">
-              <h4 class="card-title">Productos</h4>
-            </template>
-            <l-table
-              class="table-hover table-striped"
-              :columns="table.columns"
-              :data="table.data"
-            ></l-table>
+            <iframe src="http://localhost/jupiter/Productos/crud" id="embebido" frameBorder="0"></iframe>
           </card>
         </div>
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-body">
-              <h4 class="card-title">Opciones</h4>
-            </div>
-            <hr>
-            <div class="">
-              <ul class="navbar-nav ml-auto">
-                <sidebar-link to="#">
-                  <div class="Edit">
-                    <i class="card-body" role="button"><i class="nc-icon nc-settings-tool-66"></i><modalEditProduc id="modal-agregar"/></i>
-                  </div>
-                </sidebar-link>
-              </ul>
-            </div>
-            <div class="">
-              <ul class="navbar-nav ml-auto">
-                <sidebar-link to="#">
-                  <div class="">
-                    <i class="card-body" role="button"> <i class="nc-icon nc-simple-add"></i> <modalAddProduc id="modal-agregar"/></i>
-                  </div>
-                </sidebar-link>
-              </ul>
-            </div>
-          </div>
-        </div>
+     
       </div>
       
     </div>
@@ -114,5 +82,14 @@ export default {
     min-height: 11em;
     display: table-cell;
     vertical-align: middle
+  }
+
+  #card{
+    height: 500px;
+  }
+  #embebido{
+    width: 100%;
+    height: 100%;
+
   }
 </style>
