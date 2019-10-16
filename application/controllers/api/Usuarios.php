349d18c6dd176ce2de81fcbce01a93586e1460c2
JMagana <jmagana.9824@gmail.com>
2019-10-16 00:03:39 -0600

    Usuarios admin lista

---
+++ application/controllers/api/Usuarios.php
@@ -0,0 +1,44 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Usuarios extends CI_Controller {
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->db = $this->load->database('pos',true);
+        $this->load->library('grocery_CRUD');
+    }
+    
+ 
+
+    public function crud()
+    {
+        $crud = new grocery_CRUD();
+        $crud->set_table("usuario");
+        $crud->set_language("spanish");
+        $crud->unset_delete();
+        $crud->unset_print();
+        $crud->unset_export();
+        $crud->unset_clone();        
+        $crud->required_fields('nombreCompleto', 'login', 'clave', 'pin', 'rol');
+        $crud->field_type('clave', 'password');
+        $crud->field_type('rol','dropdown',
+            array('G' => 'G', 'M' => 'M'));
+        $output =$crud->render();
+        foreach ($output->css_files as $key => $css) {
+            echo ' <link rel="stylesheet" type="text/css" href="'.$css.'">';
+           }
+        foreach ($output->js_files as $key => $js) {
+            echo '<script src="'.$js.'" > </script>';
+           }
+        foreach ($output->js_lib_files as $key => $js) {
+         echo '<script src="'.$js.'" > </script>';
+        }
+       echo $output->output;
+    }
+
+}
+
+/* End of file Categorias.php */
